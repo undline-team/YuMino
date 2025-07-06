bac0b89048c2ab90be2a2b20f7ca2424dd11b39f
undline-team <supundline@gmail.com>
2025-07-06 21:46:34 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -34,27 +34,27 @@ done
 
 echo "Все пакеты успешно установлены."
 
-REPO_URL="http://f1144757.xsph.ru/YuMino_1.2.zip"
-REPO_NAME="YuMino_1.2.zip"
+REPO_URL="http://f1144757.xsph.ru/YuMino_1.3.zip"
+REPO_NAME="YuMino_1.3.zip"
 EXTRACTED_DIR="YuMino"
 
 if [ -d "$EXTRACTED_DIR" ]; then
   echo "$EXTRACTED_DIR уже существует."
-  mv "$EXTRACTED_DIR" "${EXTRACTED_DIR}.bak.$(date +%Y%m%d%H%M%S)"
+  mv "$EXTRACTED_DIR" "${EXTRACTED_DIR}_$(date +%Y%m%d%H%M%S).bak"
 fi
 
 if ! command -v unzip &> /dev/null
 then
-  echo "unzip не найден. Пожалуйста, установите unzip перед запуском скрипта. установка..."
+  echo "unzip не найден. Установка unzip..."
   pkg install unzip
   echo"unzip был установлен. скрипт завершён"
   exit 1
 fi
 
-echo "Скачивание юзербота..."
+echo "Установка юзербота..."
 wget "$REPO_URL" -O "$REPO_NAME"
 if [ $? -ne 0 ]; then
-  echo "Ошибка при скачивании юзербота."
+  echo "Ошибка при установки юзербота."
   exit 1
 fi
 echo "юзербот скачан"
@@ -63,7 +63,7 @@ echo "Распаковка репозитория..."
 unzip "$REPO_NAME" -d "$EXTRACTED_DIR"
 
 if [ $? -ne 0 ]; then
-    echo "Ошибка при распаковке юзербота.  Убедитесь, что установлен unzip и архив не поврежден."
+    echo "Ошибка при распаковке юзербота.  Убедитесь, что установлен unzip."
     rm "$REPO_NAME"
     exit 1
 fi
